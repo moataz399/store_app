fd45ce0de9c6a85205085c7343092b00dd23fa2e
moataz399 <moatazzezo41@gmail.com>
2024-05-09 01:16:42 +0300

    - native splash screen done - screen util; done

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env.variables.dart';
+import 'package:store_app/core/widgets/no_network_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,16 +10,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Scaffold(
-            appBar: AppBar(
-          title: const Text("Store App"),
-        )));
+    return ValueListenableBuilder(
+        valueListenable: ConnectivityController.instance.isConnected,
+        builder: (_, value, __) {
+          if (value) {
+            return ScreenUtilInit(
+              designSize: const Size(375, 812),
+              minTextAdapt: true,
+              child: MaterialApp(
+                title: 'Flutter Demo',
+                debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+                builder: (context, widget) {
+                  return Scaffold(
+                    body: Builder(builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    }),
+                  );
+                },
+                theme: ThemeData(
+                  colorScheme:
+                      ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                  useMaterial3: true,
+                ),
+                home: Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Store App"),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return MaterialApp(
+              title: 'No Network',
+              debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: const NoNetworkScreen(),
+            );
+          }
+        });
   }
 }
